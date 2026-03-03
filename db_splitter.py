105a64d50c4bc2e5a828abe3450a40637b75ba6a
Tapani Stylman <tapani.stylman@gofore.com>
2026-03-03 16:12:41 +0200

    working implementation

--- db_splitter.py
+++ db_splitter.py
@@ -1,10 +1,38 @@
+"""
+    Utility for partitioning simulation result database to n databases while keeping all information 
+    related to any given stand consistently within a single database.
+"""
+import argparse
+import os
 import sqlite3
 import prtpy
 
 
-def partition_db(in_db: sqlite3.Connection, out_dbs: list[sqlite3.Connection]):
-    # calculate number of tree rows for each stand
+def partition_db(in_db: sqlite3.Connection, output_db_count: int):
+    """Partition given database to output_db_count smaller databases.
+
+    Args:
+        in_db (sqlite3.Connection): open connection to simulation result database
+        output_db_count (int): number of databases to split the results into
+    """
+
     in_cur = in_db.cursor()
+
+    # initialize output databases
+    table_defs = in_cur.execute(
+        """--sql
+        SELECT sql FROM sqlite_master WHERE type='table';                   
+        """
+    ).fetchall()
+
+    for i in range(1, output_db_count + 1):
+        with sqlite3.connect(f"out_{i}.db") as out_db:
+            out_cur = out_db.cursor()
+            for table_def in table_defs:
+                out_cur.execute(table_def[0])
+            out_db.commit()
+
+    # calculate number of tree rows for each stand
     in_cur.execute(
         """--sql
         SELECT stands.identifier, COUNT(*)
@@ -12,51 +40,55 @@ def partition_db(in_db: sqlite3.Connection, out_dbs: list[sqlite3.Connection]):
         ON stands.identifier == trees.stand AND
         stands.node == trees.node
         GROUP BY stands.identifier;
-    """)
+        """
+    )
     row_counts: dict[str, int] = dict(in_cur)
 
-    # partition to len(out_dbs) bins
+    # partition to output_db_count bins
     partitioning: list[list[str]] = prtpy.partition(algorithm=prtpy.partitioning.greedy,
-                                                    numbins=len(out_dbs), items=row_counts)
+                                                    numbins=output_db_count, items=row_counts)
 
-    # Write to output dbs based on partition
-    for out_db, stands in zip(out_dbs, partitioning):
-        out_cur = out_db.cursor()
+    # write to output dbs
+    table_names = in_cur.execute(
+        """--sql
+        SELECT name FROM sqlite_master WHERE type='table';
+        """
+    ).fetchall()
 
-        # nodes
+    for i, stands in enumerate(partitioning, 1):
         in_cur.execute(
             f"""--sql
-            SELECT * FROM nodes
-            WHERE stand IN ({", ".join('?' for _ in stands)});
-            """,
-            stands
+            ATTACH DATABASE 'out_{i}.db' as out_db;
+            """
         )
-
-        out_cur.executemany(
-            """--sql
-            INSERT INTO nodes
-            VALUES (?, ?, ?, ?, ?, ?)
-            """,
-            in_cur
-        )
-
-        # stands
+        for table_ in table_names:
+            table = table_[0]
+            in_cur.execute(
+                f"""--sql
+                INSERT INTO out_db.{table}
+                SELECT * FROM main.{table}
+                WHERE main.{table}.{"stand" if table != "stands" else "identifier"}
+                IN ({', '.join('?' for _ in stands)});
+                """,
+                stands
+            )
+        in_db.commit()
         in_cur.execute(
-            f"""--sql
-            SELECT * FROM stands
-            WHERE identifier IN ({", ".join('?' for _ in stands)});
-            """,
-            stands
-        )
-
-        out_cur.executemany(
             """--sql
-            INSERT INTO stands
-            VALUES (?, ?, ?, ?, ?, ?)
-            """,
-            in_cur
+            DETACH DATABASE out_db;
+            """
         )
 
-        # trees
-        # strata
-        # collected data
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_db', type=str)
+    parser.add_argument('output_db_count', type=int)
+    args = parser.parse_args()
+
+    for j in range(1, args.output_db_count + 1):
+        if os.path.isfile(f"out_{j}.db"):
+            os.remove(f"out_{j}.db")
+
+    with sqlite3.connect(args.input_db) as in_db_:
+        partition_db(in_db_, args.output_db_count)
